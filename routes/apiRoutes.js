f202639c38a036b6fd2aa272b39bd4ea602878d2
gkpeel <gkpeel@gmail.com>
2018-11-13 23:29:16 -0500

    configured server to use react

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,23 +2,11 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
+  app.get("/api/examples", function(req, res) {});
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  app.post("/api/examples", function(req, res) {});
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  app.delete("/api/examples/:id", function(req, res) {});
 };
