4e1b13ac65131536a1810b5e44e82fa09c9d0475
gkpeel <gkpeel@gmail.com>
2018-11-14 14:29:47 -0500

    removed res.render reference modified routes to query items not example

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,10 +2,10 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {});
+  app.get("/api/items", function(req, res) {});
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {});
+  app.post("/api/item", function(req, res) {});
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {});
