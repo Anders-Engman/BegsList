119b71596df1dbac5447bba1ebf89e2d71e2152b
alexszeliga <alexszeliga@gmail.com>
2018-11-15 18:25:14 -0500

    added passport

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+
+var db = require("../models");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "userName"
+    },
+    function(userName, password, done) {
+      db.User.findOne({ where: { userName: userName } }).then(function(dbUser) {
+        if (!dbUser) {
+          return done(null, false, {
+            message: "Incorrect Username"
+          });
+        } else if (!dbUser.validPassword(password)) {
+          return done(null, false, { message: "Incorrect Password" });
+        }
+        return done(null, dbUser);
+      });
+    }
+  )
+);
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Exporting our configured passport
+module.exports = passport;
