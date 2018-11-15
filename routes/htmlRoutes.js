032f2afc4858e0d14e79256fc7f213e8d7aa8d0c
gkpeel <gkpeel@gmail.com>
2018-11-15 11:31:09 -0500

    removed react, reset handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,13 +2,28 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {});
+  app.get("/", function(req, res) {
+    db.Item.findAll({}).then(function(dbItems) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbItems
+      });
+    });
+  });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {});
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
-    res.send("404 Error - Page Not Found");
+    res.render("404");
   });
 };
