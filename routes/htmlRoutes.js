c7c3bcc2b8f5a5ceb02ddb27845ee0744e2119bd
gkpeel <gkpeel@gmail.com>
2018-11-16 13:33:24 -0500

    created gradient logic, still connecting to handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,23 +1,77 @@
 var db = require("../models");
 
-gradientByVotes = function() {};
-
+// Log Sequelize Statements as raw SQL
 function sqlLogger(msg) {
   console.log("SQL Log:");
   console.log(msg);
 }
 
-sortByItemScoreSum = function(results) {
-  results.sort(function(a, b) {
-    return b.dataValues.itemScore - a.dataValues.itemScore;
-  });
+function Color(hslObject) {
+  this.hue = hslObject.hue;
+  this.saturation = hslObject.saturation;
+  this.lightness = hslObject.lightness;
+}
+
+// Console log rank and color
+logRankAndColor = function(results) {
   for (var i = 0; i < results.length; i++) {
     console.log(
       results[i].dataValues.Item.name,
       "-",
       results[i].dataValues.itemScore,
-      "votes"
+      "votes. Color:",
+      results[i].dataValues.Color
     );
+  }
+};
+
+// Sort Sequelize Query by the Items' voteScore
+sortByItemScoreSum = function(results) {
+  results.sort(function(a, b) {
+    return b.dataValues.itemScore - a.dataValues.itemScore;
+  });
+};
+
+calcColorChange = function(topColor, bottomColor, listLength) {
+  var colorDistance = {
+    hue: topColor.hue - bottomColor.hue,
+    saturation: topColor.saturation - bottomColor.saturation,
+    lightness: topColor.lightness - bottomColor.lightness
+  };
+  var colorChangePerItem = {
+    hue: Math.floor(colorDistance.hue / (listLength - 1)),
+    saturation: Math.floor(colorDistance.saturation / (listLength - 1)),
+    lightness: Math.floor(colorDistance.lightness / (listLength - 1))
+  };
+  return colorChangePerItem;
+};
+
+setItemColor = function(startColor, hslValue, itemRank) {
+  return {
+    hue: startColor.hue - hslValue.hue * itemRank,
+    saturation: startColor.saturation - hslValue.saturation * itemRank,
+    lightness: startColor.lightness - hslValue.lightness * itemRank
+  };
+};
+
+insertItemColorVal = function(results) {
+  var itemListLength = results.length;
+  var greenest = {
+    hue: 130,
+    saturation: 87,
+    lightness: 45
+  };
+  var reddest = {
+    hue: 10,
+    saturation: 87,
+    lightness: 45
+  };
+
+  hslDeltaVal = calcColorChange(greenest, reddest, itemListLength);
+
+  for (var i = 0; i < itemListLength; i++) {
+    itemColor = setItemColor(greenest, hslDeltaVal, i);
+    results[i].dataValues.Color = new Color(itemColor);
   }
 };
 
@@ -34,6 +88,9 @@ module.exports = function(app) {
       logging: sqlLogger
     }).then(function(dbItems) {
       sortByItemScoreSum(dbItems);
+      insertItemColorVal(dbItems);
+      // console.log(dbItems);
+      // logRankAndColor(dbItems);
       res.render("index", {
         items: dbItems
       });
