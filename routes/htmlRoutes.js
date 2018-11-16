1df16f8e12c658f81dff4de8c1ca2b51bbda9878
Binh Lam <cindybinh444@gmail.com>
2018-11-16 03:26:06 -0500

    add routes for searching similar items

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,25 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     db.Item.findAll({
       include: [db.User]
-    }).then(function(dbItems) {
+    }).then(function (dbItems) {
       res.render("index", {
         items: dbItems
       });
     });
   });
 
-  app.get("/test-modal", function(req, res) {
+  //load items page (Beg input and suggested items list)
+  app.get("/items", function (req, res) {
+    db.Item.findAll({}).then(function (dbItem) {
+      res.render("items");
+    });
+  });
+
+  app.get("/test-modal", function (req, res) {
     // db.User.findAll({}).then(function(dbItems) {
     res.render("test", {
       // msg: "Welcome!",
@@ -23,8 +30,12 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function (
       dbExample
     ) {
       res.render("example", {
@@ -34,7 +45,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
