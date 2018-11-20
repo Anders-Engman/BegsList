1ccc821de6f3c76d41663b377c06d77b181dc060
gkpeel <gkpeel@gmail.com>
2018-11-20 17:19:01 -0500

    vote display persists on items and user views

--- public/js/index.js
+++ public/js/index.js
@@ -3,31 +3,52 @@ $(document).ready(function() {
   // When hamburger or 'Collapse <-' buttons are pushed, toggle '.active' class to key divs to create expanded/collapsed sidebar
   $("#sidebarToggle, #collapseToggle").on("click", function() {
     $(
-      ".layout__side-bar, .layout__main, .layout__items-list, .menu"
+      ".layout__side-bar, .layout__main, .layout__main-container, .menu"
     ).toggleClass("active");
   });
 
   // Post Vote
   $(".up-vote, .down-vote").on("click", function(event) {
-    //MUST CREATE a new user, use this section to input their User ID to test
-    var testUserId = 12;
-
-    //Prevent href="#" reload
     event.preventDefault();
-
-    //Toggle Button's Selected Class
-
-    // Make POST using the User's ID (temp Dr. Spaceman), the Item's ID, and the buttons value (1 or -1)
-    $.ajax({
-      method: "POST",
-      url: "/api/votes/",
-      data: {
-        voteValue: $(this).data("val"),
-        ItemId: $(this).data("id"),
-        UserId: testUserId
-      }
-    }).then(function(response) {
-      console.log(response);
-    });
+    var userId = $(".layout__main-container").data("user-id");
+    var voteValue = $(this).data("val");
+    var itemId = $(this).data("id");
+    if (userId) {
+      // Make POST using the User's ID (temp Dr. Spaceman), the Item's ID, and the buttons value (1 or -1)
+      $.ajax({
+        method: "POST",
+        url: "/api/votes/",
+        data: {
+          voteValue: voteValue,
+          ItemId: itemId,
+          UserId: userId
+        }
+      }).then(function(response) {
+        console.log(response);
+        console.log(voteValue);
+        console.log(response.voteValue);
+        if (Number.parseInt(response.voteValue) > 0) {
+          $(".layout__vote-content [data-id=" + response.ItemId + "]").addClass(
+            "selected-up-vote"
+          );
+          $(
+            ".layout__vote-content [data-id=" + response.ItemId + "]"
+          ).removeClass("selected-down-vote");
+        } else {
+          $(".layout__vote-content [data-id=" + response.ItemId + "]").addClass(
+            "selected-down-vote"
+          );
+          $(
+            ".layout__vote-content [data-id=" + response.ItemId + "]"
+          ).removeClass("selected-up-vote");
+        }
+      });
+    } else {
+      $("#loginModal").modal("show");
+    }
   });
+});
+
+$(".up-vote [data-id='1']").on("click", function(e) {
+  console.this.addClass("selected-up-vote");
 });
