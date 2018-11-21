512cb45af8dadfe9635f4a99c1fc833455534455
A-Engman <anders.p.engman@gmail.com>
2018-11-20 19:26:26 -0500

    Added Comment Model Functionality

--- models/comment.js
+++ models/comment.js
@@ -1,6 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
   var BegComment = sequelize.define("Comment", {
-    text: DataTypes.TEXT,
+    text: DataTypes.TEXT
   });
+
+  BegComment.associate = function(models) {
+    // Associating BegComment with User and the post
+    BegComment.belongsTo(models.User, {});
+    BegComment.belongsTo(models.Item, {});
+  };
   return BegComment;
 };
